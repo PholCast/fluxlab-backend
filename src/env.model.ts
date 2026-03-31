2dd5a44f7d911a7ae6ce2203b7e6ffcb88e34858
pholcast <pholcast17@gmail.com>
2026-03-30 19:35:45 -0500

    feat: refactor application structure and add environment configuration
    - Removed AppController and AppService as they were not needed.
    - Integrated @nestjs/config for environment variable management.
    - Set up TypeORM with PostgreSQL using async configuration.
    - Added Swagger documentation setup for API.
    - Included helmet for security enhancements.
    - Implemented global validation pipes for DTO transformation.
    - Created an Env interface for type safety on environment variables.
    - Added a .env.example file for environment variable configuration.

---
+++ src/env.model.ts
@@ -0,0 +1,8 @@
+export interface Env {
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: number;
+  POSTGRES_DB: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  JWT_SECRET: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet({
+    contentSecurityPolicy: false, // evita problemas con Swagger en dev
+  }));
+
+  app.enableCors({
+    origin: '*', // Cambiar esto al dominio específico en producción
+  });
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, //se agregó esto para cuando se use la librería transformer en los DTO 
+    }),
+  );
+
+
+  const config = new DocumentBuilder().setTitle('Fluxlab API').setDescription('Fluxlab API description').setVersion('1.0').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
